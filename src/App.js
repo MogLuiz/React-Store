3f26d651164861f041b99d762dc998d28a23ef28
Luiz Henrique <luizhjramos@outlook.com>
2021-09-03 19:28:49 -0300

    ending lifecycle methods class

--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   state = {
-    posts: []
-  }
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3'
+      },
+    ]
+  };
+  timeoutUpdate = null;
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'O título 1',
-            body: 'O corpo 1'
-          },
-          {
-            id: 2,
-            title: 'O título 2',
-            body: 'O corpo 2'
-          },
-          {
-            id: 3,
-            title: 'O título 3',
-            body: 'O corpo 3'
-          },
-        ]
-      })
-    }, 5000)
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O título mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
   }
 
   render() {
-    const { posts } = this.state
+    const { posts, counter } = this.state;
 
     return (
-          <div className="App">
-            {posts.map(post => (
-              <div key={post.id}>
-                <h1 >{post.title}</h1>
-                <p>{post.title}</p>
-              </div>
-            ))}
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
           </div>
-        );
+        ))}
+      </div>
+    );
   }
 }
-
-
-
 export default App;
